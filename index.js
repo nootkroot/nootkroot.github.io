d7df8dcc346c4daaf63b3951013224f059afaf2b
nootkroot <arielkhais@hotmail.com>
2021-04-11 11:32:47 -0700

    Add all site files

---
+++ index.js
@@ -0,0 +1,8 @@
+function openDropdown() {
+  var x = document.getElementById("Navbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
